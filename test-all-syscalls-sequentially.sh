c342e497f503c720b0a3d6becc34d0a638061147
Gnasoh <yhs2739@naver.com>
2023-06-30 16:09:27 +0900

    new file

--- test-all-syscalls-sequentially.sh
+++ test-all-syscalls-sequentially.sh
@@ -9,11 +9,10 @@
 
 DESTINATION=kataworker@192.168.122.109
 PASSWORD=1234
-PATH=/tmp/trinity2/trinity
 
 while [ 1 ]
 do
-for syscall in $($TRINITY_PATH/trinity -L | grep entrypoint | grep -v AVOID | awk '{ print $4 }' | sort -u)
+for syscall in $($TRINITY_PATH/trinity -L | grep entrypoint | grep -v AVOID | awk '{ print $3 }' | sort -u)
 do
         pushd $TRINITY_TMP
 
@@ -23,9 +22,9 @@ do
                 exit
         fi
         sshpass -p $PASSWORD ssh -t $DESTINATION "echo 1 > /sys/kernel/debug/tracing/tracing_on"
-        MALLOC_CHECK_=2 $TRINITY_PATH/trinity -c $syscall -N 1000 -C 1 $DROPPRIVS -x execve
+        MALLOC_CHECK_=2 $TRINITY_PATH/trinity -a 64 -c $syscall -l off -C 1 $DROPPRIVS
         sshpass -p $PASSWORD ssh -t $DESTINATION "echo 0 > /sys/kernel/debug/tracing/tracing_on; cat /sys/kernel/debug/tracing/trace | grep pool > /home/kataworker/ftraceresult/${syscall}.txt;"
-        sshpass -p $PASSWORD scp $PATH/tmp/${syscall}_test.txt $DESTINATION:/home/kataworker/trinityresult
+        sshpass -p $PASSWORD scp /program/${syscall}_test.txt $DESTINATION:/home/kataworker/trinityresult
 
         chmod 755 $TRINITY_TMP
         popd
